d45dfc83e99e9565e96547ff260d2a3ededd65db
Vamsi <thoyzvamsi@gmail.com>
2026-03-24 22:39:57 +0530

    Multiple_sectors

--- README.md
+++ README.md
@@ -3,45 +3,38 @@
 2 . winrate more than 52%
 3 . Tested and Trained on very large datasets Mid cap and Large cap stocks
 
-4. Stocks that i need for training
-    "RELIANCE.NS",
-    "TCS.NS",
-    "INFY.NS",
-    "HDFCBANK.NS",
-    "ICICIBANK.NS",
-    "AXISBANK.NS",
-    "KOTAKBANK.NS",
-    "HCLTECH.NS",
-    "TECHM.NS",
-    "SUNPHARMA.NS",
-    "CIPLA.NS",
-    "PIDILITIND.NS",
-    "COLPAL.NS",
-    "BHARTIARTL.NS",
-    "LT.NS",
-    "MARUTI.NS",
-    "ITC.NS,"
-    "HINDUNILVR.NS",
-    "ULTRACEMCO.NS",
-    "POWERGRID.NS",
-    "NTPC.NS",
-    "SBIN.NS",
-    "DIVISLAB.NS",
-    "BAJAJ-AUTO.NS"
+# Data Set Used
+ Stocks Date used for training are Large and Mid cap stocks for stability
 
-# accuracy given by ai model is too less (51%) so i am preparing my own feature set
-
+# Iteration 1
 # Features for logistic regression :
-
 for now i am thinking about 4 of them
 1. RSI
 2. ema_25_distance = (price - ema_25)/ema25
 3. ema_50_distance = (price - ema_50)/ema50
 4. ema_spread = (EMA25 - EMA50) / EMA50
-
 5. Target = next day price Up(1) or Down(0) 
 
-Features used to train is 1,2,3,4 
+# Problems Faced:
+1. I selected target as if last day price is lower than today's 1 if not the 0
+   instead of setting target if next day is up then 1 if not 0
 
-# it is an iterative process so we do it again and again until the accuracy is higher
+2. one of the first problem i faced is Model is just predicting majority class
+   and 61% accuracy (i got very much excited after seeing the accuracy) Because
+   (Probability Distribution : mean , min , max :0.5000619225268386 0.4987903185671936 0.5008517493880529 ) 
+   the values are very near to 50 so what does model do if proba > 0.5 = 1 
+
+# Iteration 2
+# Features for logistic regression :
+  Same as the last iteraion
+
+# Problems Faced:
+1. The indicators i am using currently (RSI ,ema_25_distance ,ema_50_distance ,ema_spread )
+   are laggy because high usage of them and got 49% accuracy
+
+2. running every cell manually will be exhustive even if they are experiments 
+   so created pipelines to run every moving part automatically 
+
+
+# it is an iterative process so we do it again and again until the accuracy got to the point i imagined
 
--- main.py
+++ main.py
@@ -3,30 +3,32 @@ from notebook.feature_engineering import Processed_Data
 from notebook.logistic_reg import Logistic_regression
 
 tickers = [
-            "RELIANCE.NS",
-            "TCS.NS",
-            "INFY.NS",
-            "HDFCBANK.NS",
-            "ICICIBANK.NS",
-            "AXISBANK.NS",
-            "KOTAKBANK.NS",
-            "HCLTECH.NS",
-            "TECHM.NS",
-            "SUNPHARMA.NS",
-            "CIPLA.NS",
-            "PIDILITIND.NS",
-            "COLPAL.NS",
-            "BHARTIARTL.NS",
-            "LT.NS",
-            "MARUTI.NS",
-            "ITC.NS,"
-            "HINDUNILVR.NS",
-            "ULTRACEMCO.NS",
-            "POWERGRID.NS",
-            "NTPC.NS",
-            "SBIN.NS",
-            "DIVISLAB.NS",
-            "BAJAJ-AUTO.NS"
+
+                    "RELIANCE.NS",
+                    "TCS.NS",
+                    "INFY.NS",
+                    "HDFCBANK.NS",
+                    "ICICIBANK.NS",
+                    "AXISBANK.NS",
+                    "KOTAKBANK.NS",
+                    "HCLTECH.NS",
+                    "TECHM.NS",
+                    "SUNPHARMA.NS",
+                    "CIPLA.NS",
+                    "PIDILITIND.NS",
+                    "COLPAL.NS",
+                    "BHARTIARTL.NS",
+                    "LT.NS",
+                    "MARUTI.NS",
+                    "ITC.NS,"
+                    "HINDUNILVR.NS",
+                    "ULTRACEMCO.NS",
+                    "POWERGRID.NS",
+                    "NTPC.NS",
+                    "SBIN.NS",
+                    "DIVISLAB.NS",
+                    "BAJAJ-AUTO.NS"
+
         ]
 
 raw_data = Raw_data(tickers)
@@ -38,4 +40,24 @@ Processed_Data_Path = Processedd_Data.Processesing_data()
 Model_Fitting = Logistic_regression(Processed_Data_Path)
 Accuracy = Model_Fitting.Logistic_Reg()
 
-print("Accuracy: ",Accuracy)
+print(f" Accuracy : {Accuracy}")
+
+
+#For multiple sectors
+
+'''
+from src.multiple_sectors import Tickers
+
+sectors = Tickers().tickers_with_sectors()
+for tickers in sectors:
+raw_data = Raw_data(tickers)
+Raw_Data_path = raw_data.Get_data()
+
+Processedd_Data = Processed_Data(Raw_Data_path)
+Processed_Data_Path = Processedd_Data.Processesing_data()
+
+Model_Fitting = Logistic_regression(Processed_Data_Path)
+Accuracy = Model_Fitting.Logistic_Reg()
+
+print(f"Sector : {tickers} and Accuracy : {Accuracy}")
+'''
